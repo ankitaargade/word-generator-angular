d089b05a483141d8d34f110d4e8e477d594bbaca
ankitaargade <argade06ankita@gmail.com>
2020-08-29 15:46:57 +0530

    first commit in word-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import wordsArray from "../utils/words";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'word-generator';
+  title = "word-generator";
+  limit: number = 10;
+
+  words = "";
+
+  onChangeInLimit(newLimit) {
+    this.limit = newLimit;
+  }
+
+  generateWords() {
+    this.words = wordsArray.slice(0, this.limit).join(" ");
+    console.log(this.words);
+  }
 }
